5050f0a6793dacad04ef28b533b734ee6165c0ec
savannah8 <willengitonga.wg@gmail.com>
2019-06-16 23:06:07 +0300

    initial commit

---
+++ photos/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+    url(r'^$',views.home_images,name='homePage'),
+    url(r'^search/', views.search_image, name='search_image'),
+    url(r'^image/(\d+)',views.image,name ='image')
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
