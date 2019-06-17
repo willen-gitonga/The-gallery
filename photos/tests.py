ddb95d08e81e874fe8b24046a2acc40be0a68cbe
savannah8 <willengitonga.wg@gmail.com>
2019-06-17 11:49:09 +0300

    updated the tests

--- photos/tests.py
+++ photos/tests.py
@@ -1,81 +1,83 @@
 from django.test import TestCase
-from .models import Location,Category,Image
 
 
+# Create your tests here.
+from .models import Location, tags, Image
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# REMEMEMBER TO QUERY ALL THE OBJECTS FROM THE DATABASE FOR TETSING EACH CLASS 
-# 
-# REMEMBER
-# REMEMBER 
 class LocationTestClass(TestCase):
-    #Set up method
+
+    #Set up method the test for location and instantiating the location object
+
 
     def setUp(self):
-        self.nairobi = Location(location_name = 'Nairobi')
+        self.test_location = Location(name = 'Nairobi')
+        self.test_location.save()
 
-    #Testing insatnce 
+    #Testing instance
 
-    def test_instance (self):
-        self.assertTrue(isinstance(self.nairobi,Location))
+    def test_instance(self):
 
+        self.assertTrue(isinstance(self.test_location, Location))
 
-    # Testing save method 
+    #Testing Save method
+
     def test_save_method(self):
-        self.nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    # Tear down method
+    def tearDown(self):
+        Location.objects.all().delete()
+
+        # Testing delete method
+
+    def test_delete_location(self):
+        self.test_location.delete()
+        self.assertEqual(len(Location.objects.all()), 0)
 
 
-class CategoryTestClass(TestCase):
-    #Set up method
-
-    def setUp(self):
-        self.food = Category(category_name = 'Food')
-
-    #Testing insatnce 
-
-    def test_instance (self):
-        self.assertTrue(isinstance(self.food,Category))
-
-
-    # Testing save method 
-    def test_save_method(self):
-        self.food.save_category()
 
 class ImageTestClass(TestCase):
-        def setUp(self):
-        #creating a new location and saving it 
+    # Set up method
+    def setUp(self):
 
-            self.nairobi = Location(location_name='Nairobi')
-            self.nairobi.save_location()
 
-        # creating a new category and saving it 
+        self.nairobi = Location.objects.create(name="nairobi")
+        self.funny= tags.objects.create(name='funny')
 
-            self.food = Category(category_name='Food')
-            self.food.save_category()
 
-        #creating a new imaage
+        self.test_image = Image.objects.create(image='imagesef',
+                                name='cat',
+                                description='This is a description',
+                                location=self.nairobi,
+                                )
 
-            self.new_image = Image(image_name ='eat',image_description='eatingafrica',image_category=self.food,image_location=self.nairobi)
-       
 
-        #Testing instance for the image 
+        # self.test_image.location.add(self.nairobi)
+        # self.test_image.tags.add(self.funny)
+        self.test_image.save()
 
-        def test_instance(self):
-            self.assertTrue(isinstance(self.new_image,Image))
+    def test_save_method(self):
+        self.test_image.save()
+        test_images = Image.objects.all()
+        self.assertTrue(len(test_images) > 0)
 
-        # Testing the save method for the Image class
-        def test_save_method(self):
-            self.new_image.save_image()
-        
-        # Testing function to get image searched by category
-        def test_get_category_image(self):
-            category_result = Image.search_by_category()
-            self.assertTrue(len(category_result)>0)
-        # Testing function to get image by id 
-        def test_get_image_by_id(self):
-            result_image_id = Image.get_image_by_id(1)
-            self.assertTrue(result_image_id.id,1)
-        
-        # Testing function to filter by location
-        def test_filter_by_location(self):
-            filtered_location = Location.objects.get(location_name='Nairobi')
-            self.assertTrue(filtered_location.location_name =='Nairobi')
+    # Testing save method
+    def test_save_image(self):
+        self.assertEqual(len(Image.objects.all()), 1)
+
+
+
+
+
+
+    # Tear down method
+    def tearDown(self):
+        Image.objects.all().delete()
+
+    def test_delete_image(self):
+        Image.delete_image_by_id(self.test_image.id)
+        self.assertEqual(len(Image.objects.all()), 0)
+
+
